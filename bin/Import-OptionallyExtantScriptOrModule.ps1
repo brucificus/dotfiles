b221e715738d4cf45320004ac87edb48867a5b0c
Bruce Markham <219281+brucificus@users.noreply.github.com>
2021-04-11 18:17:58 -0400

    🔧 Adds Skeleton for PowerShell Profile

---
+++ bin/Import-OptionallyExtantScriptOrModule.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+param (
+    [string] $path
+)
+
+function ResolvePathWithPossibleExtension([string] $path, [string] $extension)
+{
+    [string] $pathWithExtensionAppended = $path + $extension
+
+    if (Test-Path $pathWithExtensionAppended -ErrorAction SilentlyContinue) {
+        return (Resolve-Path -Path $pathWithExtensionAppended -Relative)
+    } else {
+        return $pathWithExtensionAppended
+    }
+}
+
+[string] $pathAsScriptModule = ResolvePathWithPossibleExtension -path $path -extension ".psm1"
+[string] $pathAsAssemblyModule = ResolvePathWithPossibleExtension -path $path -extension ".dll"
+[string] $pathAsScript = ResolvePathWithPossibleExtension -path $path -extension ".ps1"
+[string] $pathWithoutExtension = ResolvePathWithPossibleExtension -path $path -extension ""
+
+if (Test-Path $pathAsScriptModule -ErrorAction SilentlyContinue) {
+    Import-Module -Name $pathAsScriptModule
+} elseif (Test-Path $pathAsAssemblyModule -ErrorAction SilentlyContinue) {
+    Import-Module -Assembly $pathAsAssemblyModule
+} elseif (Test-Path $pathAsScript -ErrorAction SilentlyContinue) {
+    . "$pathAsScript"
+} elseif (Test-Path $pathWithoutExtension -ErrorAction SilentlyContinue) {
+    . "$pathWithoutExtension"
+}
---
+++ powershell/profile.ps1
@@ -0,0 +1,3 @@
+$ErrorActionPreference = "Continue"
+
+~\bin\Import-OptionallyExtantScriptOrModule.ps1 -path "~\.config\powershell\profile_local"
