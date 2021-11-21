f3d5288b47707ce7bc1d0cf483bd53da07e60eab
Bruce Markham <219281+brucificus@users.noreply.github.com>
2021-11-21 12:59:33 -0500

    ➖ Removes PowerShell Module `Az.Tools.Predictor`
    It isn't getting used and doesn't yet have stable compatibility with fluctuating versions of PSReadline and PowerShell.

--- powershell/Initialize-PSReadLine.ps1
+++ powershell/Initialize-PSReadLine.ps1
@@ -3,15 +3,8 @@ Import-Module -Name PSReadLine
 [bool] $PSReadLineSupportsOptionPredictionSource = ($null -ne $(Get-PSReadLineOption).PredictionSource)
 
 if ($PSReadLineSupportsOptionPredictionSource) {
-    [bool] $AzureCliAvailable = [bool](Get-Command az -ErrorAction SilentlyContinue)
-    [bool] $MachineIsUsedForAzureDevelopment = $AzureCliAvailable
 
-    if ($MachineIsUsedForAzureDevelopment) {
-        Import-Module Az.Tools.Predictor
-        Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-    } else {
-        Set-PSReadLineOption -PredictionSource History
-    }
+    Set-PSReadLineOption -PredictionSource History
 
     [bool] $PSReadLineSupportsOptionPredictionViewStyle = ($null -ne $(Get-PSReadLineOption).PredictionViewStyle)
 
