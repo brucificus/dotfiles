723ee6159a6deb53c011424b786de151817960a7
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-07-17 21:08:26 -0400

    🐛 Fixes `BASHIT_*` Variables for Zsh
    This makes some variables available for when the profile is starting up in Zsh-mode.

--- shell/hooks/env/bashit.bash
+++ shell/hooks/env/bashit.bash
@@ -3,14 +3,13 @@
 
 BASH_IT="$(realpath "$PWD"/../../vendor/bash-it)"; export BASH_IT
 
-if [ -z "$BASH_VERSION" ]; then
-    return 0
-fi
-
 BASHIT_ALIASES_AVAILABLE="$BASH_IT/aliases/available"; export BASHIT_ALIASES_AVAILABLE
 BASHIT_COMPLETIONS_AVAILABLE="$BASH_IT/completion/available"; export BASHIT_COMPLETIONS_AVAILABLE
 BASHIT_PLUGINS_AVAILABLE="$BASH_IT/plugins/available"; export BASHIT_PLUGINS_AVAILABLE
 
+if [ -z "$BASH_VERSION" ]; then
+    return 0
+fi
 
 # Initialize global variables for Bash-It.
 BASH_IT_LOG_PREFIX="core: main: "
