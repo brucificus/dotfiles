90ee7d3b1da81d123d7a122369b0aca93a8f2862
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-16 09:53:22 -0400

    🐛 Fixes Resolution of VSCode Shell Integration Script

--- powershell/hooks/rc/__find_vscode.ps1
+++ powershell/hooks/rc/__find_vscode.ps1
@@ -35,6 +35,34 @@ if (-not $Env:VSCODE) {
 
 phook_enqueue_module "poshy-wrap-vscode"
 
-if ($env:TERM_PROGRAM -eq "vscode") {
-    . "$(&$Env:VSCODE --locate-shell-integration-path pwsh)"
+# Check if the VS Code shell integration is already loaded.
+if (Test-Path variable:global:__VSCodeOriginalPrompt) {
+    Write-Verbose "__find_vscode.ps1: VS Code shell integration already loaded."
+	return;
+} elseif ($env:TERM_PROGRAM -eq "vscode") {
+    # Let's look for the shell integration script at its usual location, to save us from launching a node process right now.
+    $ds = [System.IO.Path]::DirectorySeparatorChar
+    [string] $vscode_dir = [System.IO.Path]::GetDirectoryName((Search-CommandPath code))
+    [string] $vscode_integration_path = Join-Path -Path $vscode_dir -ChildPath "resources${ds}app${ds}out${ds}vs${ds}workbench${ds}contrib${ds}terminal${ds}browser${ds}media${ds}shellIntegration.ps1"
+
+    if (-not (Test-Path $vscode_integration_path -ErrorAction SilentlyContinue)) {
+        # No good. Let's ask VS Code itself, then, where its script is.
+        try {
+            $vscode_integration_path = "$(&$Env:VSCODE --locate-shell-integration-path pwsh)"
+        } catch {
+            Write-Warning "__find_vscode.ps1: Failed to locate VS Code shell integration script: $_"
+            return
+        }
+        if (-not $vscode_integration_path) {
+            Write-Warning "__find_vscode.ps1: VS Code didn't return anything when asked to locate its shell integration script, exited with code '$LASTEXITCODE'."
+            return
+        }
+    } else {
+        Write-Verbose "__find_vscode.ps1: Sourcing the VS Code shell integration script found at expected location."
+    }
+    try {
+        . $vscode_integration_path
+    } catch {
+        Write-Warning "__find_vscode.ps1: Failed to source VS Code shell integration script '$vscode_integration_path': $_"
+    }
 }
