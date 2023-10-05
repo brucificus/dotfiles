83944f487423f9cadc0fbc7df029c93e6f0b09a9
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-10-05 10:27:35 -0400

    ⬆️ Updates PowerShell Dependencies

--- powershell/init.ps1
+++ powershell/init.ps1
@@ -6,9 +6,11 @@ Set-StrictMode -Version Latest
 $InformationPreference = "Continue"
 
 Import-Module -Name "$PSScriptRoot/internal-modules/poshy-phookq/" -DisableNameChecking
-phook_enqueue_folder "$PSScriptRoot/modules/poshy-env-var/" -AsFunctions
-phook_enqueue_folder "$PSScriptRoot/internal-modules/poshy-misc-funcs/" -AsFunctions
 
+phook_enqueue_folder "$PSScriptRoot/modules/poshy-lucidity/" -AsFunctions
+phook_enqueue_folder "$PSScriptRoot/modules/poshy-env-var/" -AsFunctions
+phook_enqueue_folder "$PSScriptRoot/modules/poshy-ecks/" -AsFunctions
+phook_enqueue_folder "$PSScriptRoot/internal-modules/poshy-misc-funcs/" -AsFunctions
 ./_phook_enqueue_local_before.ps1
 phook_enqueue_folder "$PSScriptRoot/hooks/${Global:phook_mode}/" -Optional
 phook_enqueue_folder "$HOME/.config/powershell/hooks_local/${Global:phook_mode}/" -Optional
